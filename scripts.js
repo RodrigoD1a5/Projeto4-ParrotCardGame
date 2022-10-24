cca6843177ad94bf4cacadacf39f8b0b6cb19dfa
RodrigoD1a5 <rodrigobastos.diass@gmail.com>
2022-10-24 10:06:29 -0300

    Versão com função para virar carta

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 var nCartas = Number(prompt("Com quantas cartas você quer jogar?"));
 const blocoCartas = document.querySelector('.bloco-cartas');
-const carta = document.querySelector('.carta');
-const inventarioCartas=[
-    "./imagens/bobrossparrot.gif","./imagens/explodyparrot.gif","./imagens/fiestaparrot.gif",
-    "./imagens/metalparrot.gif","./imagens/revertitparrot.gif","./imagens/tripletsparrot.gif",
+const cartaParrot = document.querySelector('.card');
+const inventarioCartas = [
+    "./imagens/bobrossparrot.gif", "./imagens/explodyparrot.gif", "./imagens/fiestaparrot.gif",
+    "./imagens/metalparrot.gif", "./imagens/revertitparrot.gif", "./imagens/tripletsparrot.gif",
     "./imagens/unicornparrot.gif"];
 
 
@@ -18,28 +18,26 @@ distribuirCartas();
 function distribuirCartas() {
     inventarioCartas.sort(comparador);
     cartasEscolhidasAleatoriamente = [];
-    for(let i=0; i<nCartas/2 ;i++){
+    for (let i = 0; i < nCartas / 2; i++) {
         cartasEscolhidasAleatoriamente.push(inventarioCartas[i]);
         cartasEscolhidasAleatoriamente.push(inventarioCartas[i]);
     }
     cartasEscolhidasAleatoriamente.sort(comparador);
     for (let i = 0; i < nCartas; i++) {
-        blocoCartas.innerHTML += `<div class="carta">
-        <img src="./imagens/back.png" class="parrot" />
-        <img src=${cartasEscolhidasAleatoriamente[i]} class="carta-verso" />
+        blocoCartas.innerHTML += `<div class="card" onclick="virarCarta(this)">
+        <div class="front-face face">
+          <img src="./imagens/back.png"/>
+        </div>
+        <div class="back-face face">
+          <img src="${cartasEscolhidasAleatoriamente[i]}"/>
+        </div>
     </div>`;
     }
 }
 
-
-function virar(){
-    carta.classList.add('virada');
-    carta.innerHTML
-    
+function comparador() {
+    return Math.random() - 0.5;
 }
-
-
-function comparador() { 
-	return Math.random() - 0.5; 
+function virarCarta(cartaClicada) {
+    cartaClicada.classList.add('virada');
 }
-
